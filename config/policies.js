4a100e17d9e398274a8d61996f3c0ccce3f30bd0
Sergio Lepore <sergio.d.lepore@gmail.com>
2014-08-31 16:41:53 -0300

    Update Waterlock configuration
    - Prevent the user creation on first login
    - Policy control

--- config/policies.js
+++ config/policies.js
@@ -29,7 +29,7 @@ module.exports.policies = {
   // '*': true,
 
   PingController: {
-    restricted: ['hasJsonWebToken'],
+    restricted: ['sessionAuth', 'hasJsonWebToken'],
     open: true
   }
 
