b03ef0946e4d8f505a0958f95666a03542f25a77
Sergio Lepore <sergio.d.lepore@gmail.com>
2014-09-15 15:52:07 -0300

    Add plugin and version management code
    Creation via POST and request to npm registry to fetch package metadata.

--- api/controllers/PluginController.js
+++ api/controllers/PluginController.js
@@ -7,6 +7,85 @@
  */
 
 module.exports = {
-	
+
+  /**
+   * Overridden from default blueprint.
+   *
+   * POST /plugin -> PluginController.create
+   */
+  create: function (req, res) {
+    var params = req.params.all();
+    var user = req.session.user;
+
+    // Check if the plugins exists. If so, just update versions. Otherwise, create and then
+    // update versions.
+    Plugin.findOne().where({
+      packageName: params.packageName,
+      user: user.id
+    }).exec(function(findErr, plugin) {
+      if (findErr) {
+        return ErrorManager.handleError(findErr, res);
+      }
+
+      if (!plugin) {
+        var pluginObj = {
+          name: params.name,
+          packageName: params.packageName,
+          user: user
+        };
+
+        Plugin.create(pluginObj).exec(function(createErr, plugin) {
+          if (createErr) {
+            return ErrorManager.handleError(createErr, res);
+          }
+
+          // Update info and versions for the newly created plugin
+          PluginService.updateVersionMetadata(plugin, function(updateMetadataErr, updatedPlugin) {
+            if (updateMetadataErr) {
+              if (updateMetadataErr.type == 'NPMREGNOTFOUND') { // not found on npm repo? destroy it.
+                plugin.destroy(function(destroyErr) {
+                  if (destroyErr) {
+                    sails.log.error(destroyErr);
+                  }
+                  return ErrorManager.handleError(updateMetadataErr, res);
+                });
+              } else { // other errors
+                return ErrorManager.handleError(updateMetadataErr, res);
+              }
+            } else {
+              return res.ok(updatedPlugin);
+            }
+          });
+        });
+      } else {
+        // Plugin exists, update info and versions
+        PluginService.updateVersionMetadata(plugin, function(updateMetadataErr, updatedPlugin) {
+          if (updateMetadataErr) {
+            if (updateMetadataErr.type == 'NPMREGNOTFOUND') { // not found on npm repo? destroy it.
+              plugin.destroy(function(destroyErr) {
+                if (destroyErr) {
+                  sails.log.error(destroyErr);
+                }
+                return ErrorManager.handleError(updateMetadataErr, res);
+              });
+            } else { // other errors
+              return ErrorManager.handleError(updateMetadataErr, res);
+            }
+          } else {
+            return res.ok(updatedPlugin);
+          }
+        });
+      }
+    });
+  },
+
+  /**
+   * Overridden from default blueprint.
+   *
+   * PUT /plugin/:id -> PluginController.update
+   */
+  update: function(req, res) {
+
+  }
 };
 
---
+++ api/services/ErrorManager.js
@@ -0,0 +1,17 @@
+/**
+ * Manages server responses in case of a Waterline error.
+ * If {err} is {ValidationError} -> res.badRequest
+ * Else -> res.serverError
+ *
+ * @param  Object   err      Waterline error
+ * @param  Object   response The response object
+ */
+module.exports.handleError = function(err, response) {
+  sails.log.error(err);
+
+  if (err.ValidationError || err.type == 'NPMREGNOTFOUND') {
+    return response.badRequest(err);
+  } else {
+    return response.serverError(err);
+  }
+};
