e13e6093f14f0eb659987b10a7b9f2556f23359c
Sergio Lepore <sergio.d.lepore@gmail.com>
2014-09-28 03:30:12 -0300

    Refactor the PluginController
    DRY approach on the blocks that call the "updateVersionMetadata"
    service.
    Changed the controller object to improve the legibility of code.

--- api/controllers/PluginController.js
+++ api/controllers/PluginController.js
@@ -6,141 +6,121 @@
  * @help        :: See http://links.sailsjs.org/docs/controllers
  */
 
-module.exports = {
+var controller = module.exports = {};
 
-  /**
-   * Overridden from default blueprint.
-   *
-   * POST /plugin -> PluginController.create
-   */
-  create: function (req, res) {
-    var params = req.params.all();
-    var user = req.session.user;
+/**
+ * Overridden from default blueprint.
+ *
+ * POST /plugin -> PluginController.create
+ */
+controller.create = function (req, res) {
+  var params = req.params.all();
+  var user = req.session.user;
 
-    // Check if the plugins exists. If so, just update versions. Otherwise, create and then
-    // update versions.
-    Plugin.findOne().where({
-      packageName: params.packageName,
-      user: user.id
-    }).exec(function(findErr, plugin) {
-      if (findErr)
-        return ErrorManager.handleError(findErr, res);
+  // Check if the plugins exists. If so, just update versions. Otherwise, create and then
+  // update versions.
+  Plugin.findOne().where({
+    packageName: params.packageName,
+    user: user.id
+  }).exec(function(findErr, plugin) {
+    if (findErr)
+      return ErrorManager.handleError(findErr, res);
 
-      if (!plugin) {
-        var pluginObj = {
-          name: params.name,
-          packageName: params.packageName,
-          user: user
-        };
+    if (!plugin) {
+      var pluginObj = {
+        name: params.name,
+        packageName: params.packageName,
+        user: user
+      };
 
-        Plugin.create(pluginObj).exec(function(createErr, plugin) {
-          if (createErr)
-            return ErrorManager.handleError(createErr, res);
+      Plugin.create(pluginObj).exec(function(createErr, plugin) {
+        if (createErr)
+          return ErrorManager.handleError(createErr, res);
 
-          // Update info and versions for the newly created plugin
-          PluginService.updateVersionMetadata(plugin, function(updateMetadataErr, updatedPlugin) {
-            if (updateMetadataErr) {
-              if (updateMetadataErr.type == 'NPMREGNOTFOUND') { // not found on npm repo? destroy it.
-                plugin.destroy(function(destroyErr) {
-                  if (destroyErr)
-                    sails.log.error(destroyErr);
-
-                  return ErrorManager.handleError(updateMetadataErr, res);
-                });
-              } else { // other errors
-                return ErrorManager.handleError(updateMetadataErr, res);
-              }
-            } else {
-              return res.emberOk(Plugin, updatedPlugin, {status:201});
-            }
-          });
-        });
-      } else {
-        // Plugin exists, update info and versions
-        PluginService.updateVersionMetadata(plugin, function(updateMetadataErr, updatedPlugin) {
-          if (updateMetadataErr) {
-            if (updateMetadataErr.type == 'NPMREGNOTFOUND') { // not found on npm repo? destroy it.
-              plugin.destroy(function(destroyErr) {
-                if (destroyErr)
-                  sails.log.error(destroyErr);
-
-                return ErrorManager.handleError(updateMetadataErr, res);
-              });
-            } else { // other errors
-              return ErrorManager.handleError(updateMetadataErr, res);
-            }
-          } else {
-            return res.emberOk(Plugin, updatedPlugin, {status:201});
-          }
-        });
-      }
-    });
-  },
-
-  /**
-   * Overridden from default blueprint.
-   *
-   * PUT /plugin/:id -> PluginController.update
-   */
-  update: function(req, res) {
-    var params = req.params.all();
-    var user = req.session.user;
-
-    Plugin.findOne().where({
-      id: params.id
-    }).exec(function(findErr, plugin) {
-      if (findErr)
-        return ErrorManager.handleError(findErr, res);
-
-      if (plugin.user != user.id)
-        return res.forbidden('You have no permissions to perform this action.');
-
-      //only the name can be manually changed, all other data comes from npm
-      plugin.name = params.name;
-
-      PluginService.updateVersionMetadata(plugin, function(updateMetadataErr, updatedPlugin) {
-        if (updateMetadataErr) {
-          if (updateMetadataErr.type == 'NPMREGNOTFOUND') { // not found on npm repo? destroy it.
-            plugin.destroy(function(destroyErr) {
-              if (destroyErr)
-                sails.log.error(destroyErr);
-
-              return ErrorManager.handleError(updateMetadataErr, res);
-            });
-          } else { // other errors
-            return ErrorManager.handleError(updateMetadataErr, res);
-          }
-        } else {
-          return res.emberOK(Plugin, updatedPlugin);
-        }
+        // Update info and versions for the newly created plugin
+        return updatePluginMetadata(res, plugin);
       });
-    });
-  },
+    } else {
+      // Plugin exists, update info and versions
+      return updatePluginMetadata(res, plugin);
+    }
+  });
+};
 
-  /**
-   * Overridden from default blueprint.
-   *
-   * DELETE /plugin/:id -> PluginController.destroy
-   */
-  destroy: function(req, res) {
-    var params = req.params.all();
-    var user = req.session.user;
+/**
+ * Overridden from default blueprint.
+ *
+ * PUT /plugin/:id -> PluginController.update
+ */
+controller.update = function(req, res) {
+  var params = req.params.all();
+  var user = req.session.user;
 
-    Plugin.findOne().where({
-      id: params.id
-    }).exec(function(findErr, plugin) {
-      if (findErr)
+  Plugin.findOne().where({
+    id: params.id
+  }).exec(function(findErr, plugin) {
+    if (findErr)
+      return ErrorManager.handleError(findErr, res);
+
+    if (plugin.user != user.id)
+      return res.forbidden('You have no permissions to perform this action.');
+
+    //only the name can be manually changed, all other data comes from npm
+    plugin.name = params.name;
+
+    return updatePluginMetadata(res, plugin);
+  });
+};
+
+/**
+ * Overridden from default blueprint.
+ *
+ * DELETE /plugin/:id -> PluginController.destroy
+ */
+controller.destroy = function(req, res) {
+  var params = req.params.all();
+  var user = req.session.user;
+
+  Plugin.findOne().where({
+    id: params.id
+  }).exec(function(findErr, plugin) {
+    if (findErr)
+      return ErrorManager.handleError(err, res);
+
+    if (plugin.user != user.id)
+      return res.forbidden('You have no permissions to perform this action.');
+
+    plugin.destroy(function(destroyErr) {
+      if (destroyErr)
         return ErrorManager.handleError(err, res);
 
-      if (plugin.user != user.id)
-        return res.forbidden('You have no permissions to perform this action.');
-
-      plugin.destroy(function(destroyErr) {
-        if (destroyErr)
-          return ErrorManager.handleError(err, res);
-
-        return res.ok(null);
-      });
+      return res.ok(null);
     });
-  },
+  });
 };
+
+/**
+ * Updates the plugin metadata, by calling the PluginService (which gets all
+ * metadata from npm registry) and handles errors and success operations.
+ *
+ * @param  Object res          The Sails Response object
+ * @param  Object pluginObject A Plugin object, populated from the database
+ */
+function updatePluginMetadata(res, pluginObject) {
+  PluginService.updateVersionMetadata(pluginObject, function(updateMetadataErr, updatedPlugin) {
+    if (updateMetadataErr) {
+      if (updateMetadataErr.type == 'NPMREGNOTFOUND') { // not found on npm repo? destroy it.
+        pluginObject.destroy(function(destroyErr) {
+          if (destroyErr)
+            sails.log.error(destroyErr);
+
+          return ErrorManager.handleError(updateMetadataErr, res);
+        });
+      } else { // other errors
+        return ErrorManager.handleError(updateMetadataErr, res);
+      }
+    } else {
+      return res.emberOk(Plugin, updatedPlugin);
+    }
+  });
+}
