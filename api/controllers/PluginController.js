db9d831d145a2a5040a4af02098b4b46598dc893
Sergio Lepore <sergio.d.lepore@gmail.com>
2014-10-01 00:01:52 -0300

    Refactor action descriptions. Add a "find" example

--- api/controllers/PluginController.js
+++ api/controllers/PluginController.js
@@ -13,7 +13,7 @@ var controller = module.exports = {};
 /**
  * Overridden from default blueprint.
  *
- * POST /plugin -> PluginController.create
+ * POST /plugins -> PluginController.create
  */
 controller.create = function (req, res) {
   var params = emberUtils.parseValues(req, Plugin);
@@ -52,7 +52,7 @@ controller.create = function (req, res) {
 /**
  * Overridden from default blueprint.
  *
- * PUT /plugin/:id -> PluginController.update
+ * PUT /plugins/:id -> PluginController.update
  */
 controller.update = function(req, res) {
   var pk = emberUtils.requirePk(req);
@@ -76,7 +76,7 @@ controller.update = function(req, res) {
 /**
  * Overridden from default blueprint.
  *
- * DELETE /plugin/:id -> PluginController.destroy
+ * DELETE /plugins/:id -> PluginController.destroy
  */
 controller.destroy = function(req, res) {
   var pk = emberUtils.requirePk(req);
@@ -125,3 +125,59 @@ function updatePluginMetadata(res, pluginObject, options) {
     }
   });
 }
+
+/**
+ * I will keep this action here just for references. Everything it does can be
+ * achieved with a simple "GET /plugins" with a query parameter named "where" and
+ * with a content like this:
+ *
+ * {
+ *   "or": [
+ *     { "name": { "contains": "404" } },
+ *     { "name": { "contains": "error" } },
+ *     { "description": { "contains": "404" } },
+ *     { "description": { "contains": "error" } },
+ *     { "packageName": { "contains": "404" } },
+ *     { "packageName": { "contains": "error" } },
+ *     { "keywordCache": { "contains": "404" } },
+ *     { "keywordCache": { "contains": "error" } },
+ *     { "readme": { "contains": "404 error" } }
+ *   ]
+ * }
+ *
+ * This JSON will be processed by Waterline.
+ *
+ * GET /plugins/search
+ */
+// controller.search = function(req, res) {
+//   var params = req.params.all();
+//   var searchQuery = params.q || '';
+//   var limit = params.size || sails.config.search.limit;
+//   var page = params.page || 1;
+//   // var queryWords = '%' + searchQuery.trim().split(/\s+/).join('%') + '%';
+//   var queryWords = searchQuery.trim().split(/\s+/);
+//   var orClause = [];
+
+//   if (!searchQuery)
+//     return res.emberOk(Plugin, []);
+
+//   queryWords.forEach(function(word) {
+//     orClause.push({ name: { 'contains': word } });
+//     orClause.push({ packageName: { 'contains': word } });
+//     orClause.push({ description: { 'contains': word } });
+//     orClause.push({ keywordCache: { 'contains': word } });
+//   });
+
+//   orClause.push({ readme: { 'contains': searchQuery } });
+
+//   Plugin.find().where({
+//     or: orClause
+//   })
+//   .paginate({ page: page, limit: limit })
+//   .exec(function(err, pluginMatches) {
+//     if (err)
+//       return ErrorManager.handleError(err, res);
+
+//     return res.emberOk(Plugin, pluginMatches);
+//   });
+// };
